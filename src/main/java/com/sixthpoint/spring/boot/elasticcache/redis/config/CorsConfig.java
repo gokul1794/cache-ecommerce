ad2f0afea2e83bcebbb53ec92544aceee9aa3fd7
Gokul Shanth <gokul.shanth@gmail.com>
2020-05-02 01:56:18 -0400

    Cross Origin annotation

--- src/main/java/com/sixthpoint/spring/boot/elasticcache/redis/config/CorsConfig.java
+++ src/main/java/com/sixthpoint/spring/boot/elasticcache/redis/config/CorsConfig.java
@@ -7,14 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                .allowedHeaders("*");
     }
 }
